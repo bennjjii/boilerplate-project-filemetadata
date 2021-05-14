c4ff7addf20315dd7688271ad460d58b8c8eae59
bennjjii <>
2021-05-14 23:22:25 +0000

    Implement file metadata microservice

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+var multer = require('multer')
+var upload = multer()
 
 var app = express();
 
@@ -11,7 +13,16 @@ app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post('/api/fileanalyse', upload.any(),
+(req, res)=>{
+  let response = {
+    name: req.files[0].originalname,
+    type: req.files[0].mimetype,
+    size: parseInt(req.files[0].size)
+  }
+  console.log(response);
+  res.json(response);
+})
 
 
 const port = process.env.PORT || 3000;
